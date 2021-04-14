7fc633bad61778a0e1263739d6cd93662689569b
moses1418 <mosesrodriguez1414@yahoo.com>
2021-04-14 13:11:56 -0500

    april 14

---
+++ JS/external.js
@@ -0,0 +1,15 @@
+"ues strict";
+
+alert("Welcome to my website");
+
+var userColor = prompt("What is your favorite color?");
+
+// alert("Your Favorite color is: " + userColor)
+
+if(userColor === "orange"){
+    alert("That is my Favorite also!")
+}else{
+    alert("Your Favorite color is: " + userColor)
+}
+
+
---
+++ JS/ok-maybe-hello-world.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// alert("I swear I won't make you see 'hello world'");
+//
+// alert("I hope my dogs don't get out again :/ ");
+
+// alert("I sharpened my blade")
+//
+// alert("I learned to write code")
+//
+// alert("Today was the day")
+
+
+// Confirm
+
+// var userChoice = confirm("Would you like to continue?");
+//
+//
+// if (userChoice){
+//     console.log("lets continue")
+// }else{
+//     comsole.log("Alright, be like that...")
+// }
+
+
+//prompt
+
+var userInput = prompt()
