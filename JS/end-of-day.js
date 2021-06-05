fba2dccec74b4e5659ae8149d0b9520a563dc809
moses1418 <mosesrodriguez1414@yahoo.com>
2021-06-05 16:31:21 -0500

    adding jquery

---
+++ JS/end-of-day.js
@@ -0,0 +1,53 @@
+// //make sure the DOM is loaded first
+// document.addEventListener("DOMContentLoaded", function() {
+//     addListeners();
+// });
+// //parent method for adding all listeners
+// function addListeners(){
+//     //get the elements to add listener and change image
+//     let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
+//     addEvents(cardContainers, 'assets/jazz-music-rubber-duck.jpg', 'mouseover');
+//     addEvents(cardContainers, 'assets/question.png', 'mouseout');
+// }
+// //add mouseover/mouseout events to all selected elements
+// function addEvents(cardContainers, imgPath, listenerType){
+//     cardContainers.forEach((cc) => {
+//         let card = cc;
+//         //create listener which will call function change the image
+//         let listener = function (event) {
+//             let cardImg = card.querySelector(".card .card-img-top");
+//             changeImage(cardImg, imgPath);
+//         };
+//         cc.addEventListener(listenerType, listener);
+//     });
+// }
+// //actual function to swap images
+// function changeImage(card, imgPath){
+//     card.setAttribute('src', imgPath);
+// }
+
+
+//------------------------
+
+$(document).ready(function (){
+    $("#submitBtn").click(function (){
+        $("#override-bootstrap").css("background-color", "red");
+    })
+})
+
+$(document).ready(function (){
+    $(".card-text").click(function (){
+        $(this).css("background-color", "red");
+    })
+})
+
+$("#resetBtn").click(function (){
+    location.reload();
+})
+
+
+$("#main-title").click(function (){
+    $(this).css("font-size", "40px")
+})
+
+
