ad1d925efe49be58b4aeb6b4c875b862fb11163d
moses1418 <mosesrodriguez1414@yahoo.com>
2021-06-09 12:34:23 -0500

    adding mapbox

---
+++ JS/mapbox-playground.js
@@ -0,0 +1,70 @@
+const mapboxToken = "pk.eyJ1IjoibW9zZXMxNCIsImEiOiJja3BwbmZ5c3AwMm1mMm5wZnMybzc2bnhuIn0.mrl-hvtqsqahg6M5m0DFng"
+mapboxgl.accessToken = mapboxToken;
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/navigation-night-v1',
+    zoom: 10,
+    center: [-98.4916, 29.4252]
+});
+
+let marker = setMarker([-98.4916, 29.4252]);
+addMapEvent(marker);
+
+let geoCoder = setGeocoder();
+addGeocoderToMap(geoCoder);
+addGeocoderEvent(geoCoder)
+
+
+function setGeocoder(){
+    return new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false
+    })
+}
+
+function addGeocoderToMap(geocoder) {
+    map.addControl(geocoder)
+}
+
+function addGeocoderEvent(geocoder){
+    geocoder.on('result', function (event) {
+        marker.setLngLat(event.result.geometry.coordinates)
+    })
+}
+
+function setMarker(point) {
+
+    let marker = new mapboxgl.Marker().setLngLat(point)
+                                            .addTo(map)
+
+
+
+
+}
+
+function addMapEvent(marker){
+    map.on("click", function (event) {
+        marker.setLngLat(event.LngLat)
+                            .addTo(map)
+    })
+}
+
+
+function setPopup() {
+    let popup = new mapboxgl.Popup()
+        .setHTML(`<p>${textDetails}</p>`)
+        .addTo(map)
+
+    marker.setPopup(popup)
+}
+
+
+
+
+
+
+
+
+
