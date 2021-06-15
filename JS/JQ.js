6a34021a244b6f2de1f552fad9234b21f5d29d1e
moses1418 <mosesrodriguez1414@yahoo.com>
2021-06-15 11:24:13 -0500

    adding exercise

--- JS/JQ.js
+++ JS/JQ.js
@@ -6,9 +6,9 @@ $("span").hide()
 
 $(".show-me").show()
 
-$(".pink").css("background-color", "pink")
+// $(".pink").css("background-color", "pink")
 
-$(".orange").css("background-color", "orange")
+$("h1", "h2").css("background-color", "orange")
 
 $(".container> div:nth-child(1)").append("<img src='https://www.rd.com/wp-content/uploads/2020/04/GettyImages-694542042-e1586274805503.jpg' height='50px' width='600px' >")
 $("#pink").css("background-color", "pink");
@@ -20,3 +20,4 @@ $(document).ready( function () {
     $("#pink").hide()
 })
 
+$
