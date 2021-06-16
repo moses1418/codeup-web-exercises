661454282db269f0572187ebdd3043f14e120e2b
moses1418 <mosesrodriguez1414@yahoo.com>
2021-06-16 09:15:47 -0500

    promise

---
+++ JS/promise.js
@@ -0,0 +1,12 @@
+// TODO: using fetch(), make a simple GET request to this api:
+//  “https://dog.ceo/api/breeds/image/random” and append this image to the image element with an id of “random-dog-image”
+
+
+.fetch("https:dog.ceo/api/breeds/image/random")
+    .then(response => respponse.json())
+    .then(data =>{
+        console.log(data);
+        console.log(data.message);
+        $("#random-dog-image").attr("src", data.message);
+    })
+    .catch(error => console.error(error));
